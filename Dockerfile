d4d018d64ee9a2640451f19d190ecb2f37bbcc56
Augusto Rocha <delphigu@gmail.com>
2023-09-13 08:13:34 -0300

    first

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:17-alpine
+	ENV APP_NAME aws
+	COPY ./target/*.jar  /app/aws.jar
+	WORKDIR /app
+	ENTRYPOINT ["java","-jar","aws.jar"]
+	EXPOSE 8080
---
+++ src/main/java/com/aws/controllers/AwsController.java
@@ -0,0 +1,18 @@
+package com.aws.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inicio")
+public class AwsController {
+	
+	@GetMapping
+	public ResponseEntity<String> teste() {
+		return ResponseEntity.ok("ola aws");
+		
+	}
+
+}
